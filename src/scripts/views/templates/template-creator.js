3c77da77d9e27fa0499ea187a10308e09ddbb8f6
adityanegara <aditnegarabisnis@gmail.com>
2021-08-03 16:09:02 +0700

    Fixing Favorite Button

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -79,13 +79,13 @@ const createRestaurantDetailTemplate = (restaurant) => `
 
 const createLikeButtonTemplate = () => `
   <button aria-label="like this restaurant" id="likeButton" class="like">
-     <i class="fa fa-heart-o" aria-hidden="true"></i>
+     Add Favorite
   </button>
 `;
 
 const createLikedButtonTemplate = () => `
-  <button aria-label="unlike this restaurant" id="likeButton" class="like">
-    <i class="fa fa-heart" aria-hidden="true"></i>
+  <button aria-label="unlike this restaurant" id="likeButton" class="liked">
+     Remove Favorite
   </button>
 `;
 
