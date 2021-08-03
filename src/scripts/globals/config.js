332854d0a871547f29d1b166468b68a6f0c0f1f4
adityanegara <aditnegarabisnis@gmail.com>
2021-08-03 14:37:53 +0700

    Implementing Cache Strategy

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   KEY: '12345',
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  CACHE_NAME: 'ROKU-V1',
 };
 
 export default CONFIG;
